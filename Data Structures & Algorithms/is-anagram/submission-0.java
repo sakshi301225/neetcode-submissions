9b8efbe691229e851aec4057d93595c616b5b391
Sakshi Bhasin <sbhasin3@buffalo.edu>
2026-06-23 22:19:38 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //brute force method
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        char c[]=s.toCharArray();
+        char d[]=t.toCharArray();
+        Arrays.sort(c);
+        Arrays.sort(d);
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]!=d[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
